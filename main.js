2c719fc0605e4824edbebd317205b8db0607e49b
Your name <Your email>
2017-07-22 02:03:24 -0400

    modified the bio section

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-
-
 let container = document.querySelector('#apiInfo');
-
 
 let request = new XMLHttpRequest();
 request.addEventListener("load", displayGitHubInfo);
 request.open('GET', 'https://api.github.com/users/jamiebort');
 request.send();
-
 
 function displayGitHubInfo() {
 
@@ -16,11 +12,13 @@ function displayGitHubInfo() {
 
   document.getElementById('name').innerHTML = `Name: ` + data.name;
 
-  document.getElementById('url').innerHTML = `URL: ` +  data.url;
+  document.getElementById('url').innerHTML = `URL: ` + data.url;
 
-  document.getElementById('email').innerHTML = `Email: ` +  data.email;
+  document.getElementById('email').innerHTML = `Email: ` + data.email;
 
-   document.getElementById('company').innerHTML = `Company: ` +  data.company;
+  document.getElementById('company').innerHTML = `Company: ` + data.company;
 
-    document.getElementById('blog').innerHTML = `Website: ` +  data.blog;
+  document.getElementById('blog').innerHTML = `Website: ` + data.blog;
+
+  document.getElementById('bio').innerHTML = data.bio;
 }
