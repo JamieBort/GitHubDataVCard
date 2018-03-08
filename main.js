fd57625afbdded632dc6afe685eb710aaac7be71
Jamie Bort <jamiebort@gmail.com>
2018-03-08 12:32:40 -0500

    Finished main.js file.

---
+++ main.js
@@ -0,0 +1,14 @@
+let request = new XMLHttpRequest();
+request.addEventListener("load", displayGitHubInfo);
+request.open('GET', 'https://api.github.com/users/jamiebort');
+request.send();
+
+function displayGitHubInfo() {
+  let data = JSON.parse(this.responseText);
+  document.getElementById('name').innerHTML=`Name: ` + data.name;
+  document.getElementById('url').innerHTML=`URL: ` + data.url;
+  document.getElementById('email').innerHTML=`Email: ` + data.email;
+  document.getElementById('company').innerHTML=`Company: ` + data.company;
+  document.getElementById('blog').innerHTML=`Website: ` + data.blog;
+  document.getElementById('bio').innerHTML=data.bio;
+}
